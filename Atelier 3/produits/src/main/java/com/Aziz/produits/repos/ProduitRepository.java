6814f28679b17dedb3de098d37a320693a2414f9
Aziz-Abdellaoui <85045660+Aziz-Abdellaoui@users.noreply.github.com>
2021-05-31 12:01:29 +0100

    Add files via upload

---
+++ Atelier 3/produits/src/main/java/com/Aziz/produits/repos/ProduitRepository.java
@@ -0,0 +1,9 @@
+package com.Aziz.produits.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Aziz.produits.entities.Produit;
+
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+
+}
